6acea839dbb017f98f39e4021d809333aca42c7c
Davy <306048104+bugsintheweb@users.noreply.github.com>
2026-08-17 12:32:32 -0500

    Modified firewall connections to accommodate SAMBA Changes to be committed:         modified:   hosts/HWY66/configuration.nix

--- hosts/HWY66/configuration.nix
+++ hosts/HWY66/configuration.nix
@@ -216,10 +216,14 @@
   services.openssh.enable = false;
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
+   networking.firewall.allowedTCPPorts = [ 
+     445
+     139
+     137
+   ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+   networking.firewall.enable = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
