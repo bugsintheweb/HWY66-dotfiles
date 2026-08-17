9b83fe50ce6a7698757689a5b2b7fc2cd0f4ccc9
Davy <306048104+bugsintheweb@users.noreply.github.com>
2026-08-17 11:16:57 -0500

    Add 7zip & Samba configuration for VMs Changes to be committed: 	modified:   hosts/HWY66/configuration.nix

--- hosts/HWY66/configuration.nix
+++ hosts/HWY66/configuration.nix
@@ -109,6 +109,28 @@
       };
     };
 
+  services.samba = {
+    enable = true;
+
+    # Samba global config (often enough to get started)
+    securityType = "user";
+
+    # Make sure samba can read the directory you want to share
+    shares = {
+      "shared" = {
+        path = "/srv/shared";
+        browseable = "yes";
+        "read only" = "no";
+        "guest ok" = "no";
+
+        # Optional but useful:
+        "create mask" = "0664";
+        "directory mask" = "0775";
+        "force user" = "davy";
+      };
+    };
+  };
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
@@ -133,6 +155,9 @@
   librewolf
   chromium
 
+  # 7zip for compression/decompression needs
+  p7zip
+  
   # Proton VPN (very redundant) 
   proton-vpn
   
